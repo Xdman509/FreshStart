7d15563b75e2eac50a3211409037ff928119f049
Xdman509 <xdman509@gmail.com>
2023-10-11 21:56:46 -0500

    ejs file

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/weather-info',function(req,res,next){
+  res.render('weather');
+});
 module.exports = router;
