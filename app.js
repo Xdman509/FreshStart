ef65759bd32267ad29deace746484484c930141b
Xdman509 <xdman509@gmail.com>
2023-10-11 21:37:07 -0500

    fixed code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var weatherRouter = require('/routes/weather');
+var weatherRouter = require('./routes/weather');
 var app = express();
 
 // view engine setup
--- routes/weather.js
+++ routes/weather.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/list', function(req, res, next) {
   let weatherData = [{
     Temp: '90.5',
     Unit: 'fahrenheit',
     Cloudy :'false',
-    PrecipitationChance: '0'
+    PrecipitationChance: '0',
 }]
 res.send(weatherData);
 });
-
 module.exports = router;
